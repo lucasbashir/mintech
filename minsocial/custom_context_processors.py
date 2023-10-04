1d0a87121926472165297cf7687f5e96b858420a
samsonlukman <lucassamson24@gmail.com>
2023-10-04 03:28:24 +0100

    sorting dark mode

--- minsocial/custom_context_processors.py
+++ minsocial/custom_context_processors.py
@@ -15,20 +15,31 @@ def my_groups_and_joined(request):
         'groups_i_joined': groups_i_joined,
     }
 
-@login_required
+
+
 def user_connections(request):
-    user = User.objects.get(pk=request.user.id)
+    friends = []  # Initialize the variable with an empty list
+    
+    if request.user.is_authenticated:
+        try:
+            user = User.objects.get(pk=request.user.id)
 
-    # Get users followed by the current user
-    following = Follow.objects.filter(follower=user).values_list('following', flat=True)
+            # Get users followed by the current user
+            following = Follow.objects.filter(follower=user).values_list('following', flat=True)
 
-    # Get users who are following the current user
-    followers = Follow.objects.filter(following=user).values_list('follower', flat=True)
+            # Get users who are following the current user
+            followers = Follow.objects.filter(following=user).values_list('follower', flat=True)
 
-    # Find users who both follow the current user and are followed by the current user
-    friend_ids = set(following).intersection(followers)
+            # Find users who both follow the current user and are followed by the current user
+            friend_ids = set(following).intersection(followers)
 
-    # Fetch the User objects for the friends
-    friends = User.objects.filter(id__in=friend_ids)
+            # Fetch the User objects for the friends
+            friends = User.objects.filter(id__in=friend_ids)
+        
+        except User.DoesNotExist:
+            pass  # Handle the case where the user does not exist
+        except Exception as e:
+            # Handle any other exceptions that might occur during the query
+            pass
 
     return {'friends': friends}
